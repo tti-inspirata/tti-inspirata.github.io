a3733a35f850321ee463d2049b1f16cf19beaeb4
TTi & Inspirata <39482694+tti-inspirata@users.noreply.github.com>
2024-01-25 11:45:38 +0800

    add logo

--- books/login/script.js
+++ books/login/script.js
@@ -3,16 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 function setLanguageBasedOnBrowser() {
-    const lang = navigator.language || navigator.userLanguage; 
+    const lang = navigator.language || navigator.userLanguage;
     if (lang.startsWith('zh')) {
-        document.getElementById('invalid-account').textContent = '用户名和密码无效!';
+        document.getElementById('invalid-account').textContent = '邮箱地址或密码错误，请重新输入。';
         document.getElementById('log-out').textContent = '账户已退出!';
         document.getElementById('login-title').textContent = '使用恩道电子书账户登录';
         document.getElementById('username').placeholder = '邮箱地址';
         document.getElementById('password').placeholder = '密码';
         document.querySelector('#login-form input[type="submit"]').value = '登录';
     } else {
-        document.getElementById('invalid-account').textContent = 'Invalid username and password.';
+        document.getElementById('invalid-account').textContent = 'Invalid login details. Please try again.';
         document.getElementById('log-out').textContent = 'You have been logged out.';
         document.getElementById('login-title').textContent = 'Log in with your Inspirata eBooks account';
         document.getElementById('username').placeholder = 'Email address';
