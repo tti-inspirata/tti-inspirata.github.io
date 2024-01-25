76704a0420c8b1651d43864645e655781879cbf3
TTi & Inspirata <39482694+tti-inspirata@users.noreply.github.com>
2024-01-25 15:09:22 +0800

    增加繁体判断

--- books/login/script.js
+++ books/login/script.js
@@ -11,7 +11,15 @@ function setLanguageBasedOnBrowser() {
         document.getElementById('username').placeholder = '邮箱地址';
         document.getElementById('password').placeholder = '密码';
         document.querySelector('#login-form input[type="submit"]').value = '登录';
-    } else {
+    } else if (lang.startsWith ('zh-Hant')){
+        document.getElementById('invalid-account').textContent = '郵箱地址或密碼錯誤，請重新輸入。';
+        document.getElementById('log-out').textContent = '賬戶已登出!';
+        document.getElementById('login-title').textContent = '使用恩道電子書賬戶登入';
+        document.getElementById('username').placeholder = '郵箱地址';
+        document.getElementById('password').placeholder = '密碼';
+        document.querySelector('#login-form input[type="submit"]').value = '登入';
+    }
+    else {
         document.getElementById('invalid-account').textContent = 'Invalid login details. Please try again.';
         document.getElementById('log-out').textContent = 'You have been logged out.';
         document.getElementById('login-title').textContent = 'Log in with your Inspirata eBooks account';
